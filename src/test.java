9a348f03866e59b30133e863075b67600d573468
MarkZhang1985 <zhangkaiming1985@icloud.com>
2018-07-28 16:48:29 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+	public static void main(String[] args) {
+		String s0 ="hello";
+		String s1 = "hello";
+		String s2 = "he" + "llo";
+		System.out.println(s0 ==s1);
+		System.out.println(s0 ==s2);
+
+		int a = (int)9.9;
+		System.out.println("output:");
+		System.out.println('\u0000');
+		System.out.println('\u1122');
+
+
+	}
+}
